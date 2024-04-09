6b27baff91d779918ec2eec6e4989e197a43a941
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-09 22:56:23 +0545

    setup drizzle schema, drizzle push config with drizzle-kit, setup drizzle studio

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'drizzle-kit'
+import { env } from './environment'
+export default defineConfig({
+    schema: "./src/db/schema.ts",
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: env.DATABASE_URL,
+    },
+    verbose: true,
+    strict: true,
+})
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '../../environment';
+import * as schema from './schema';
 
 // for query purposes
 const queryClient = postgres(env.DATABASE_URL);
-const db = drizzle(queryClient);
+const db = drizzle(queryClient, { schema });
 
 export default db
