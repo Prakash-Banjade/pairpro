4c52fdaa72daf8ed9bc9aca2a9c272c706657faa
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-16 01:00:16 +0545

    room delete dialog added

--- src/app/home/components/room-delete-btn.tsx
+++ src/app/home/components/room-delete-btn.tsx
@@ -1,8 +1,21 @@
 'use client'
 
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import toast from "react-hot-toast"
 import { deleteRoom } from "../action"
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { CircleBackslashIcon, TrashIcon } from "@radix-ui/react-icons"
+
 
 type Props = {
     roomId: string,
@@ -21,6 +34,29 @@ export default function RoomDeleteBtn({ roomId }: Props) {
     }
 
     return (
-        <Button variant={'destructive'} onClick={handleDelete}>Delete</Button>
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant={'destructive'}>Delete</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Sure to delete room?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>
+                        <CircleBackslashIcon className="mr-2 h-4 w-4" />
+                        Cancel
+                        </AlertDialogCancel>
+                    <AlertDialogAction className={buttonVariants({ variant: 'destructive' })} onClick={handleDelete}>
+                        <TrashIcon className="mr-2 h-4 w-4" />
+                        Delete
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+
     )
 }
