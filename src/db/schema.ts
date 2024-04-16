72444f9352cac5bddb389ab7878e8501195321a2
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-16 21:06:31 +0545

    added allowedUsers column on room with M:N relationship

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { boolean, pgEnum, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { boolean, pgEnum, pgTable, primaryKey, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const oAuthProviderEnum = pgEnum('oAuth', ['google', 'github', 'linkedin'])
 export const visibility = pgEnum('visibility', ['public', 'private'])
@@ -20,6 +20,7 @@ export const users = pgTable('users', {
 
 export const usersRelations = relations(users, ({ many }) => ({
     createdRooms: many(room),
+    allowedRooms: many(allowedUsersOnRoom),
 }))
 
 export type NewUser = typeof users.$inferInsert
@@ -40,7 +41,20 @@ export const room = pgTable('room', {
 
 export const roomRelations = relations(room, ({ many, one }) => ({
     creator: one(users, { fields: [room.creatorId], references: [users.id] }),
+    allowedUsers: many(allowedUsersOnRoom),
 }));
+
+export const allowedUsersOnRoom = pgTable('allowedUsersOnRoom', { // join table for users that are allowed to join a room since M:N relationship
+    roomId: uuid('roomId').notNull().references(() => room.id, { onDelete: 'cascade' }),
+    userId: uuid('userId').notNull().references(() => users.id, { onDelete: 'cascade' }),
+}, (table) => ({
+    pk: primaryKey({ columns: [table.roomId, table.userId] }), // no need for separate pk, roomId and userId are unique, so the combination acts as primary key
+}))
+
+export const allowedUsersOnRoomRelations = relations(allowedUsersOnRoom, ({ one }) => ({
+    user: one(users, { fields: [allowedUsersOnRoom.userId], references: [users.id] }),
+    room: one(room, { fields: [allowedUsersOnRoom.roomId], references: [room.id] }),
+}))
 
 export type NewRoom = typeof room.$inferInsert
 export type Room = typeof room.$inferSelect
