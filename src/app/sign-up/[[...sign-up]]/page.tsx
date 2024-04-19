6d722ca4f8644dd75eb231614e0c5fa498aba616
Prakash Banjade <prakashbanjade191@gmail.com>
2024-04-19 08:14:59 +0545

    modified signup page

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
- 
+
 export default function SignUpPage() {
-  return <SignUp />;
+  return <div className=" h-screen">
+    <section className="flex justify-center pt-20 h-full">
+      <SignUp />
+    </section>
+  </div>
 }
